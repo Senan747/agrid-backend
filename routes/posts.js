9e3c691574782b45825ad364f2194c179b8e329a
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-10-15 02:03:44 +0400

    crud

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/", postsController.save);
 router.get("/:id", postsController.show);
 router.get("/", postsController.index);
+router.delete("/:id", postsController.destroy)
 
 module.exports = router;
